364ccae1af6e35ff3611faad41f6c46df37ec4f3
ElijahCoding <cuiyeqing960904@gmail.com>
2020-06-28 18:15:01 +0300

    merged

--- src/Weight.php
+++ src/Weight.php
@@ -18,9 +18,6 @@ class Weight
 
     public function toLbs(): float
     {
-<<<<<<< HEAD
         return $this->kilograms * 2.204623;
-=======
->>>>>>> ee3fc97f78bc2fb8dac81d3e3777bd05d82161e4
     }
 }
